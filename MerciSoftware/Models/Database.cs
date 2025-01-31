a8421584dcbe0d686ca64bd405a63166037b1d39
Daniele Berardinelli <berardinellidaniele@protonmail.com>
2025-01-31 15:00:00 +0100

    first commit

---
+++ MerciSoftware/Models/Database.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using MerciSoftware.Models;
+
+namespace MerciSoftware.Models
+{
+    public class Database
+    {
+        private readonly string _conn;
+
+        public Database(IConfiguration configuration)
+        {
+            _conn = configuration.GetConnectionString("Default");
+        }
+
+        private SqlConnection CreaConnessione()
+        {
+            return new SqlConnection(_conn);
+        }
+
+
+
+
+    }
+}
